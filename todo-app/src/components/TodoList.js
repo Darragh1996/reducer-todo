97448bce18cce59ee54088defb1569b64d220a6c
Darragh Ferry <darraghferry@hotmail.com>
2019-12-10 20:45:21 +0000

    added line-through style to completed tasks

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -2,7 +2,7 @@ import React, { useReducer } from "react";
 // import { initialState, reducer } from "../reducers/reducer";
 
 export default function TodoList(props) {
-//   const [state, dispatch] = useReducer(reducer, initialState);
+  //   const [state, dispatch] = useReducer(reducer, initialState);
 
   const markTodo = (id, completed) => event =>
     props.dispatch({
@@ -15,7 +15,13 @@ export default function TodoList(props) {
   return (
     <div className="component">
       {props.state.todoList.map(todo => (
-        <div key={todo.id} style={{ color: !todo.completed ? "red" : "green" }}>
+        <div
+          key={todo.id}
+          style={{
+            color: !todo.completed ? "red" : "green",
+            textDecorationLine: todo.completed ? "line-through" : ""
+          }}
+        >
           {todo.name}
           <button onClick={markTodo(todo.id, true)}>Mark complete</button>
           <button onClick={markTodo(todo.id, false)}>Mark incomplete</button>
